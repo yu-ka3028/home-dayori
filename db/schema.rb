c3d4245ee373d76a718548aa8e038a51109421f1
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-06-09 20:21:29 +0900

    投票制限

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_07_034736) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_09_105932) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "radio_name"
     t.text "content"
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_07_034736) do
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_votes_on_post_id"
   end
 
