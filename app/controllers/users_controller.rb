63def48763072c52c6b230449743b4720634ddf3
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-06-12 23:56:14 +0900

    新規登録後、投稿画面へ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to new_post_path
     else
       render :new
     end
