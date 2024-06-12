75df06f4a3ece3923aebcd9835397a1b0231bba1
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-06-12 16:30:49 +0900

    軽微修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   validates :radio_name, :content, presence: true # radio_name, content は必須
   has_many :votes, dependent: :destroy # 投票は投稿が削除されたら一緒に削除
+  belongs_to :user, optional: true # 投稿はユーザーに紐づくが、ユーザーが削除されても投稿は残す
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_many :posts
-  authenticates_with_sorcery!
-  validates :password, length: { minimum: 4 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  has_many :posts, dependent: :destroy # ユーザーが削除されたら投稿も一緒に削除
+  authenticates_with_sorcery! # sorcery で認証機能を使う
+  validates :password, length: { minimum: 4 }, if: -> { new_record? || changes[:crypted_password] } 
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] } 
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] } 
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
